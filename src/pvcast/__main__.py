870727770e4753be4c3f6771afee6293cd02dd9f
Stefan <langestefan@msn.com>
2023-05-31 13:37:30 +0000

    Add new source files

---
+++ src/pvcast/__main__.py
@@ -0,0 +1,10 @@
+"""Module that contains the command line application."""
+
+
+def main():
+    """Entry point for the application script"""
+    print("Hello world!")
+
+
+if __name__ == "__main__":
+    main()
