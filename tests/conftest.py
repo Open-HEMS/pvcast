bf99c5b6f922abb21831940744a7467cf4aba1d3
Stefan <langestefan@msn.com>
2023-06-05 23:11:17 +0000

    Add API tests and error handling

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,7 +12,9 @@ See https://stackoverflow.com/questions/34466027/in-pytest-what-is-the-use-of-co
 
 from __future__ import annotations
 
+from dataclasses import replace
 import pytest
+import unittest.mock as mock
 
 from pvcast.weather.weather import WeatherAPIClearOutside
 
@@ -22,7 +24,24 @@ LOC_AUS = (-31.9741, 115.8517, 0.0)
 
 
 # check with different lat/lon/alt
-@pytest.fixture(autouse=True, params=[LOC_EUW, LOC_USW, LOC_AUS])
+@pytest.fixture(params=[LOC_EUW, LOC_USW, LOC_AUS])
 def weather_co(request):
     """Fixture for the Clear Outside weather API."""
     return WeatherAPIClearOutside(*request.param)
+
+
+@pytest.fixture()
+def weather_co_wrong_url():
+    """Fixture for the Clear Outside weather API with no data."""
+    obj = WeatherAPIClearOutside(*LOC_EUW)
+
+    # set the url to a wrong url (require replace() bc dataclass is frozen)
+    return replace(obj, url_base="https://clearoutside.com/wrongcast/")
+
+
+@pytest.fixture()
+def weather_co_too_many_req():
+    """Fixture for clear outside response 429 (too many requests)."""
+    # set the url to a wrong url (require replace() bc dataclass is frozen)
+    obj = replace(WeatherAPIClearOutside(*LOC_EUW), url_base="http://httpbin.org/status/429", format_url=False)
+    return obj
--- tests/test_weather.py
+++ tests/test_weather.py
@@ -5,11 +5,28 @@
 """This is a sample python file for testing functions from the source code."""
 from __future__ import annotations
 
+import pytest
 
-class TestWeather:
-    """Test the weather module."""
+from pvcast.weather.weather import WeatherAPIErrorNoData, WeatherAPIErrorTooManyReq
+from pandas import DataFrame
+
+
+class TestWeatherCO:
+    """Test the clear outside weather module."""
 
     def test_get_weather_co(self, weather_co):
         """Test the get_weather function."""
         weather = weather_co.get_weather()
-        assert isinstance(weather, dict)
+        assert isinstance(weather, DataFrame)
+
+    def test_get_weather_wrong_url(self, weather_co_wrong_url):
+        """Test the get_weather function."""
+        with pytest.raises(WeatherAPIErrorNoData) as excinfo:
+            weather_co_wrong_url.get_weather()
+        assert "No weather data available" in str(excinfo.value.message)
+
+    def test_get_weather_too_many_req(self, weather_co_too_many_req):
+        """Test the get_weather function."""
+        with pytest.raises(WeatherAPIErrorTooManyReq) as excinfo:
+            weather_co_too_many_req.get_weather()
+        assert "Too many requests" in str(excinfo.value.message)
