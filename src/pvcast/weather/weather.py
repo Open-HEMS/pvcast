ce8227d56b0cb81d4f1ec409226375f7b043102c
Stefan <langestefan@msn.com>
2023-07-30 19:02:09 +0000

    Move unit conversion to parent weather class

--- src/pvcast/weather/weather.py
+++ src/pvcast/weather/weather.py
@@ -33,6 +33,38 @@ WEATHER_SCHEMA = Schema(
     }
 )
 
+# temperature conversion functions ("°C", "°F", "C", "F")
+TEMP_CONV_DICT = {
+    "F": {
+        "C": lambda x: (5 / 9) * (x - 32),
+    },
+    "C": {
+        "C": lambda x: x,
+    },
+}
+
+# speed conversion functions ("m/s", "km/h", "mi/h", "ft/s", "kn")
+SPEED_CONV_DICT = {
+    "m/s": {
+        "km/h": lambda x: x * 3.6,
+    },
+    "km/h": {
+        "m/s": lambda x: x / 3.6,
+    },
+    "mi/h": {
+        "m/s": lambda x: x / 2.23694,
+    },
+    "ft/s": {
+        "m/s": lambda x: x / 3.28084,
+    },
+    "kn": {
+        "m/s": lambda x: x / 1.94384,
+    },
+}
+
+# combine temperature and speed conversion dictionaries
+CONV_DICT = {**TEMP_CONV_DICT, **SPEED_CONV_DICT}
+
 
 @dataclass
 class WeatherAPI(ABC):
@@ -94,6 +126,31 @@ class WeatherAPI(ABC):
         return datetime strings and in that case this index is not needed and even not preferred.
         """
         return pd.date_range(self.start_forecast, self.end_forecast, freq=self.freq_source, tz="UTC")
+
+    @staticmethod
+    def convert_unit(data: pd.Series, from_unit: str, to_unit: str) -> pd.Series:
+        """Convert units of a pd.Series.
+
+        :param data: The data to convert. This should be a pd.Series.
+        :param to_unit: The unit to convert to.
+        :return: Data with applied unit conversion.
+        """
+        if not isinstance(data, pd.Series):
+            raise TypeError("Data must be a pd.Series.")
+
+        # remove degree symbol from units if present
+        from_unit = from_unit.replace("°", "")
+        to_unit = to_unit.replace("°", "")
+
+        if from_unit not in CONV_DICT.keys():
+            raise ValueError(f"Conversion from unit [{from_unit}] not supported.")
+        if from_unit == to_unit:
+            return data
+        if to_unit not in CONV_DICT[from_unit].keys():
+            raise ValueError(f"Conversion from [{from_unit}] to [{to_unit}] not supported.")
+
+        # do unit conversion
+        return data.apply(CONV_DICT[from_unit][to_unit])
 
     @abstractmethod
     def _process_data(self) -> pd.DataFrame:
