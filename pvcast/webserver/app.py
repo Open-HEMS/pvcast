33692aecfb0b93f2a1593909a0f4869e97e009bc
Stefan <langestefan@msn.com>
2023-09-20 22:27:12 +0000

    Change docs URL

--- pvcast/webserver/app.py
+++ pvcast/webserver/app.py
@@ -10,21 +10,21 @@ from .const import API_VERSION
 from .routers.clearsky import router as clearsky_router
 
 _LOGGER = logging.getLogger("uvicorn")
-
+FAV_ICON_PATH = "pvcast/webserver/favicon.png"
 
 app = FastAPI(
     title="PV Cast",
     description="A webserver for the PV Cast project.",
     version=API_VERSION,
+    docs_url="/",
+    redoc_url=None,
 )
 
 app.include_router(clearsky_router, prefix="/clearsky", tags=["clearsky"])
-
-favicon_path = "pvcast/webserver/favicon.png"
 
 
 @app.get("/favicon.ico", include_in_schema=False)
 async def favicon():
     """Get the favicon."""
     _LOGGER.info("Getting favicon")
-    return FileResponse(favicon_path)
+    return FileResponse(FAV_ICON_PATH)
