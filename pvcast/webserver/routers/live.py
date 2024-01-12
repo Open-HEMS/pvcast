3432164ea96fcd305feea90b5b7cab154a77187b
Stefan <langestefan@msn.com>
2024-01-12 09:18:10 +0000

    Add optional start and end timestamps for weather data filtering to live

--- pvcast/webserver/routers/live.py
+++ pvcast/webserver/routers/live.py
@@ -6,11 +6,11 @@ from typing import Any
 
 import polars as pl
 from fastapi import APIRouter, Depends
-from typing_extensions import Annotated
+from typing_extensions import Annotated, Optional
 
 from ...model.model import PVSystemManager
 from ...weather.weather import WeatherAPI
-from ..models.base import Interval, PVPlantNames
+from ..models.base import Interval, PVPlantNames, StartEndRequest
 from ..models.live import LiveModel, WeatherSources
 from ..routers.dependencies import get_pv_system_mngr, get_weather_sources
 from .helpers import get_forecast_result_dict
@@ -26,6 +26,7 @@ def post(
     weather_source: WeatherSources,
     pv_system_mngr: Annotated[PVSystemManager, Depends(get_pv_system_mngr)],
     weather_apis: Annotated[list[WeatherAPI], Depends(get_weather_sources)],
+    start_end: Optional[StartEndRequest] | None = None,
     interval: Interval = Interval.H1,
 ) -> LiveModel:
     """Get the estimated PV output power in Watts and energy in Wh at the given interval <interval> \
@@ -59,6 +60,13 @@ def post(
     weather_df = pl.DataFrame(weather_dict["data"]).with_columns(
         pl.col("datetime").str.to_datetime()
     )
+
+    # filter weather data between start and end timestamps
+    if start_end is not None:
+        weather_df = weather_df.filter(
+            (weather_df["datetime"] >= start_end.start)
+            & (weather_df["datetime"] <= start_end.end)
+        )
 
     # get the PV power output
     response_dict = get_forecast_result_dict(
